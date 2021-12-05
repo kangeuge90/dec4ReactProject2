b65e9373cbe9b4f7f9d09b91b09ad0d8c76916e1
Eugene Kang <kangeuge90@gmail.com>
2021-12-05 14:33:19 -0500

    process of adding getType

--- src/DisplayCuisine.tsx
+++ src/DisplayCuisine.tsx
@@ -13,28 +13,30 @@ function DisplayCuisine() {
     const [query, setQuery] = useState('');
 
     return(
-    <div className="categoryPicturesContainer">
-        <div className="categoryPicture" onClick = {() => {setQuery('American')}}>
-            <h3>American</h3>
-            <img src={american} alt="american cuisine"> </img>
-        </div>
-        <div className="categoryPicture" onClick = {() => {setQuery('Asian')}}>
-            <h3>Asian</h3>
-            <img src={asian} alt="asian cuisine"></img>
-        </div>
-        <div className="categoryPicture" onClick = {() => {setQuery('Indian')}}>
-            <h3>Indian</h3>
-            <img src={indian} alt="indian cuisine"></img>
-        </div>
-        <div className="categoryPicture" onClick = {() => {setQuery('Italian')}}>
-            <h3>Italian</h3>
-            <img src={italian} alt="italian cuisine"></img>
-        </div>
-        <div className="categoryPicture" onClick = {() => {setQuery('Mexican')}}>
-            <h3>Mexican</h3>
-            <img src={mexican} alt="mexican cuisine"></img>
-        </div>
-        <hr></hr>
+    <div>
+        <div className="categoryPicturesContainer">
+            <div className="categoryPicture" onClick = {() => {setQuery('American')}}>
+                <h3>American</h3>
+                <img src={american} alt="american cuisine"></img>
+            </div>
+            <div className="categoryPicture" onClick = {() => {setQuery('Asian')}}>
+                <h3>Asian</h3>
+                <img src={asian} alt="asian cuisine"></img>
+            </div>
+            <div className="categoryPicture" onClick = {() => {setQuery('Indian')}}>
+                <h3>Indian</h3>
+                <img src={indian} alt="indian cuisine"></img>
+            </div>
+            <div className="categoryPicture" onClick = {() => {setQuery('Italian')}}>
+                <h3>Italian</h3>
+                <img src={italian} alt="italian cuisine"></img>
+            </div>
+            <div className="categoryPicture" onClick = {() => {setQuery('Mexican')}}>
+                <h3>Mexican</h3>
+                <img src={mexican} alt="mexican cuisine"></img>
+            </div>
+        </div>    
+        <hr></hr>    
         <div className='recipeList'>
             <Recipe query={query}/>
         </div>
--- src/GetRecipe.ts
+++ src/GetRecipe.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { type } from 'os';
 import { useState } from 'react';
 
 export interface RecipeResponse {
@@ -19,7 +20,7 @@ export interface RecipeItem {
 
 
 const getRecipe = async(query: string) => {
-    const type = "";
+    const t = "";
     const q = query;
     const app_id = "";
     const app_key = "";
@@ -35,5 +36,24 @@ const getRecipe = async(query: string) => {
     
       
 }
+
+const getType = async(query: string) => {
+    const t = type;
+    const q = '';
+    const app_id = "";
+    const app_key = "";
+
+    const url = (`https://api.edamam.com/api/recipes/v2?type=${type}&q=&app_id=5469f054&app_key=79d6e8b780f2ca65225e9566f8ac5b28`)
+    const response = await axios.get(url,
+    {params: {type, q, app_id, app_key}});
+    console.log(response.data.hits);
+    return (
+        response.data.hits.map((item: any) => {
+            return{...item.recipe, thumbNail: item.recipe.images?.REGULAR?.url || 'No Image Available'}})
+    )
+    
+      
+}
+
 
 export default getRecipe;
